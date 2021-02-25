96e67ab6a0345e6aad6fce01d3187a6ee37219e4
Sinhika <sinhika@republicofnewhome.org>
2021-02-25 13:48:36 -0600

    removed main/resources loot_tables for blocks because they are to be generated in generated/resources instead.

---
+++ src/main/java/mod/akkamaddi/classicalalchemy/datagen/ClassicalDataGenerator.java
@@ -0,0 +1,37 @@
+package mod.akkamaddi.classicalalchemy.datagen;
+
+import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
+
+import mod.akkamaddi.classicalalchemy.ClassicalAlchemy;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+/**
+ * bundles up the GatherDataEvent handler and all the necessary data providers for
+ * data generation.
+ * @author Sinhika
+ */
+@EventBusSubscriber(modid = ClassicalAlchemy.MODID, bus = MOD)
+public class ClassicalDataGenerator
+{
+    /**
+     * GatherDataEvent handler.
+     * @param event the GatherDataEvent.
+     */
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event)
+    {
+        DataGenerator gen = event.getGenerator();
+        if (event.includeServer())
+        {
+            gen.addProvider(new Recipes(gen));
+            gen.addProvider(new FusionRecipes(gen));
+            gen.addProvider(new ModBlockTags(gen, event.getExistingFileHelper()));
+            gen.addProvider(new ModItemTags(gen, event.getExistingFileHelper()));
+            gen.addProvider(new ClassicalLootTableProvider(gen));
+        }
+     } // end gatherData()
+
+} // end class
---
+++ src/main/java/mod/akkamaddi/classicalalchemy/datagen/ClassicalLootTableProvider.java
@@ -0,0 +1,38 @@
+package mod.akkamaddi.classicalalchemy.datagen;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import com.mojang.datafixers.util.Pair;
+
+import mod.akkamaddi.classicalalchemy.init.ModBlocks;
+import mod.alexndr.simplecorelib.datagen.AbstractLootTableProvider;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.loot.LootParameterSet;
+import net.minecraft.loot.LootTable.Builder;
+import net.minecraft.util.ResourceLocation;
+
+public class ClassicalLootTableProvider extends AbstractLootTableProvider
+{
+
+    public ClassicalLootTableProvider(DataGenerator dataGeneratorIn)
+    {
+        super(dataGeneratorIn);
+    }
+
+    @Override
+    protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, Builder>>>, LootParameterSet>> getTables()
+    {
+        tables.clear();
+        standardDropTable(ModBlocks.stannum_block.get());
+        standardDropTable(ModBlocks.cuprum_block.get());
+        standardDropTable(ModBlocks.pyropus_bronze_block.get());
+        standardDropTable(ModBlocks.pulchrum_bronze_block.get());
+        standardDropTable(ModBlocks.tomb_bronze_block.get());
+        
+        return tables;
+    } // end getTables()
+
+} // end class
