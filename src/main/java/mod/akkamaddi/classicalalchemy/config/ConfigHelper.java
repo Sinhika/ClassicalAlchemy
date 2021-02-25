e234c2567ca0051f987add4ccb08d62fb9868afc
Sinhika <sinhika@republicofnewhome.org>
2021-02-25 10:47:24 -0600

    drafted basic configuration stuff

--- src/main/java/mod/akkamaddi/classicalalchemy/config/ConfigHelper.java
+++ src/main/java/mod/akkamaddi/classicalalchemy/config/ConfigHelper.java
@@ -13,5 +13,15 @@ public final class ConfigHelper
         // recipe flags.
         ClassicalAlchemyConfig.INSTANCE.putFlag("recycling_enabled", 
                 ConfigHolder.SERVER.serverEnableRecycling.get());
+        ClassicalAlchemyConfig.INSTANCE.putFlag("stannum_making", 
+                ConfigHolder.SERVER.serverEnableStannumMaking.get());
+        ClassicalAlchemyConfig.INSTANCE.putFlag("cuprum_making", 
+                ConfigHolder.SERVER.serverEnableCuprumMaking.get());
+        ClassicalAlchemyConfig.INSTANCE.putFlag("pyropus_bronze_making", 
+                ConfigHolder.SERVER.serverEnablePyropusBronzeMaking.get());
+        ClassicalAlchemyConfig.INSTANCE.putFlag("pulchrum_bronze_making", 
+                ConfigHolder.SERVER.serverEnablePulchrumBronzeMaking.get());
+        ClassicalAlchemyConfig.INSTANCE.putFlag("tomb_bronze_making", 
+                ConfigHolder.SERVER.serverEnableTombBronzeMaking.get());
     } // end bakeServer()
 } // end class
--- src/main/java/mod/akkamaddi/classicalalchemy/config/ServerConfig.java
+++ src/main/java/mod/akkamaddi/classicalalchemy/config/ServerConfig.java
@@ -6,18 +6,51 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public final class ServerConfig
 {
     final ForgeConfigSpec.BooleanValue serverEnableRecycling; // recipe flag
+    final ForgeConfigSpec.BooleanValue serverEnableStannumMaking; // recipe flag
+    final ForgeConfigSpec.BooleanValue serverEnableCuprumMaking; // recipe flag
+    final ForgeConfigSpec.BooleanValue serverEnablePyropusBronzeMaking; // recipe flag
+    final ForgeConfigSpec.BooleanValue serverEnablePulchrumBronzeMaking; // recipe flag
+    final ForgeConfigSpec.BooleanValue serverEnableTombBronzeMaking; // recipe flag
     
 
     ServerConfig(final ForgeConfigSpec.Builder builder)
     {
         builder.push("General");
         serverEnableRecycling = 
-                builder.comment("Enable fusion furnace recycling of bronze items")
+                builder.comment("Enable fusion furnace recycling of mod items")
                 .translation(ClassicalAlchemy.MODID + "config.EnableRecycling")
                 .define("EnableRecycling", true);
         builder.pop();
-        
-
+        builder.push("Stannum");
+        serverEnableStannumMaking =
+                builder.comment("Enable fusion furnace creation of stannum items")
+                .translation(ClassicalAlchemy.MODID + "config.EnableStannumMaking")
+                .define("EnableStannumMaking", true);
+        builder.pop();
+        builder.push("Cuprum");
+        serverEnableCuprumMaking = 
+            builder.comment("Enable fusion furnace creation of cuprum items")
+            .translation(ClassicalAlchemy.MODID + "config.EnableCuprumMaking")
+            .define("EnableCuprumMaking", true);
+        builder.pop();
+        builder.push("Pyropus Bronze");
+        serverEnablePyropusBronzeMaking = 
+                builder.comment("Enable fusion furnace creation of pyropus bronze items")
+                .translation(ClassicalAlchemy.MODID + "config.EnablePyropusBronzeMaking")
+                .define("EnablePyropusBronzeMaking", true);
+        builder.pop();
+        builder.push("Pulchrum Bronze");
+        serverEnablePulchrumBronzeMaking = 
+                builder.comment("Enable fusion furnace creation of pulchrum bronze items")
+                .translation(ClassicalAlchemy.MODID + "config.EnablePulchrumBronzeMaking")
+                .define("EnablePulchrumBronzeMaking", true);
+        builder.pop();
+        builder.push("Tomb Bronze");
+        serverEnableTombBronzeMaking = 
+                builder.comment("Enable fusion furnace creation of tomb bronze items (if Arsenic & Lace mod loaded)")
+                .translation(ClassicalAlchemy.MODID + "config.EnableTombBronzeMaking")
+                .define("EnableTombBronzeMaking", true);
+        builder.pop();
     } // end ctor()
     
 } // end class
