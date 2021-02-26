29360ee1af44f1fcbe54d1830bc47fff071a5794
Sinhika <sinhika@republicofnewhome.org>
2021-02-26 14:02:48 -0600

    fixed typo in generate_silent script; added silent recipes generated by script; edited tomb_bronze recipes for alloy_smelter to also require 'arsenic' mod to be loaded.

--- scripts/generate_silent.sh
+++ scripts/generate_silent.sh
@@ -33,24 +33,24 @@ make_silents_recipes.py --type=crusher --ingredient="${ID}:tomb_bronze_ingot" \
 # alloy smelter
 # cuprum
 make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/copper" \
-    "#forge:dusts/copper" "#minecraft:coals" \ 
+    "#forge:dusts/copper" "#minecraft:coals" \
     --result "${ID}:cuprum_nugget" -- cuprum_nugget
 make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/copper" \
-    "#forge:dusts/copper" "minecraft:bone_meal" \ 
+    "#forge:dusts/copper" "minecraft:bone_meal" \
     --result "${ID}:medium_cuprum_chunk" -- medium_cuprum_chunk
 make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/copper" \
-    "#forge:dusts/copper" "minecraft:gunpowder" \ 
+    "#forge:dusts/copper" "minecraft:gunpowder" \
     --result "${ID}:large_cuprum_chunk" -- large_cuprum_chunk
 
 # stannum
 make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/tin" \
-    "#forge:dusts/tin" "#minecraft:coals" \ 
+    "#forge:dusts/tin" "#minecraft:coals" \
     --result "${ID}:stannum_nugget" -- stannum_nugget
 make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/tin" \
-    "#forge:dusts/tin" "minecraft:bone_meal" \ 
+    "#forge:dusts/tin" "minecraft:bone_meal" \
     --result "${ID}:medium_stannum_chunk" -- medium_stannum_chunk
 make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/tin" \
-    "#forge:dusts/tin" "minecraft:gunpowder" \ 
+    "#forge:dusts/tin" "minecraft:gunpowder" \
     --result "${ID}:large_stannum_chunk" -- large_stannum_chunk
 
 # pyropus_bronze
@@ -58,15 +58,15 @@ make_silents_recipes.py --type=alloy_smelter --ingredient "${ID}:stannum_dust" \
     "${ID}:cuprum_dust" "minecraft:bone_meal" \
     --result "${ID}:pyropus_bronze_nugget" -- pyropus_bronze_nugget
 make_silents_recipes.py --type=alloy_smelter --ingredient "${ID}:stannum_dust" \
-    "${ID}:cuprum_dust"  "minecraft:gunpowder" \ 
+    "${ID}:cuprum_dust"  "minecraft:gunpowder" \
     --result "${ID}:medium_pyropus_bronze_chunk" -- medium_pyropus_bronze_chunk
 make_silents_recipes.py --type=alloy_smelter --ingredient "${ID}:stannum_dust" \
-    "${ID}:cuprum_dust"  "minecraft:redstone" \ 
+    "${ID}:cuprum_dust"  "minecraft:redstone" \
     --result "${ID}:large_pyropus_bronze_chunk" -- large_pyropus_bronze_chunk
 
 # pulchrum_bronze
 make_silents_recipes.py --type=alloy_smelter --ingredient "${ID}:pyropus_bronze_dust" \
-    "minecraft:gold_ingot"  "minecraft:redstone" \ 
+    "minecraft:gold_ingot"  "minecraft:redstone" \
     --result "${ID}:large_pulchrum_bronze_chunk" -- large_pulchrum_bronze_chunk
 
 # tomb_bronze
@@ -74,9 +74,9 @@ make_silents_recipes.py --type=alloy_smelter --ingredient "${ID}:pyropus_bronze_
     "arsenic:arsenide_bronze_dust" "minecraft:rotten_flesh" \
     --result "${ID}:tomb_bronze_nugget" -- tomb_bronze_nugget
 make_silents_recipes.py --type=alloy_smelter --ingredient "${ID}:pyropus_bronze_dust" \
-    "arsenic:arsenide_bronze_dust"  "minecraft:redstone" \ 
+    "arsenic:arsenide_bronze_dust"  "minecraft:redstone" \
     --result "${ID}:medium_tomb_bronze_chunk" -- medium_tomb_bronze_chunk
 make_silents_recipes.py --type=alloy_smelter --ingredient "${ID}:pyropus_bronze_dust" \
-    "arsenic:arsenide_bronze_dust"  "minecraft:glowstone_dust" \ 
+    "arsenic:arsenide_bronze_dust"  "minecraft:glowstone_dust" \
     --result "${ID}:large_tomb_bronze_chunk" -- large_tomb_bronze_chunk
 
