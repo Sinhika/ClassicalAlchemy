0d87e326200e77b7f4bc327a2da8f27a6414d3c2
Sinhika <sinhika@republicofnewhome.org>
2021-05-16 11:33:45 -0500

    added skeleton of loot table injection lookup, and LootLoad event

--- src/main/java/mod/akkamaddi/classicalalchemy/ForgeEventSubscriber.java
+++ src/main/java/mod/akkamaddi/classicalalchemy/ForgeEventSubscriber.java
@@ -3,10 +3,14 @@ package mod.akkamaddi.classicalalchemy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import mod.akkamaddi.classicalalchemy.config.ClassicalAlchemyConfig;
 import mod.akkamaddi.classicalalchemy.content.ClassicalArmorMaterial;
+import mod.akkamaddi.classicalalchemy.loot.ClassicalAlchemyInjectionLookup;
 import mod.alexndr.simplecorelib.helpers.ArmorUtils;
+import mod.alexndr.simplecorelib.helpers.LootUtils;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,6 +20,21 @@ import net.minecraftforge.fml.common.Mod;
 public final class ForgeEventSubscriber
 {
     private static final Logger LOGGER = LogManager.getLogger(ClassicalAlchemy.MODID + " Forge Event Subscriber");
+    private static final ClassicalAlchemyInjectionLookup lootLookupMap = new ClassicalAlchemyInjectionLookup();
+   
+    /**
+     * add mod loot to loot tables. Code heavily based on Botania's LootHandler, which
+     * neatly solves the problem when I couldn't figure it out.
+     */
+    @SubscribeEvent
+    public static void LootLoad(final LootTableLoadEvent event)
+    {
+        if (ClassicalAlchemyConfig.addChestLoot)
+        {
+            LootUtils.LootLoadHandler(ClassicalAlchemy.MODID, event, lootLookupMap);
+        } // end-if config allows
+    } // end LootLoad()
+
     
     @SubscribeEvent(receiveCanceled = true, priority= EventPriority.HIGHEST)
     public static void onLivingAttackEvent(LivingAttackEvent event)
---
+++ src/main/java/mod/akkamaddi/classicalalchemy/loot/ClassicalAlchemyInjectionLookup.java
@@ -0,0 +1,15 @@
+package mod.akkamaddi.classicalalchemy.loot;
+
+import mod.alexndr.simplecorelib.helpers.InjectionTableLookup;
+
+@SuppressWarnings("serial")
+public class ClassicalAlchemyInjectionLookup extends InjectionTableLookup
+{
+
+    public ClassicalAlchemyInjectionLookup()
+    {
+        super();
+        // TODO
+    }
+
+} // end class
