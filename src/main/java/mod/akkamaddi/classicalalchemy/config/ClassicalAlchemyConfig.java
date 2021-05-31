54c054fd1eaa7e8f493a8e6d61028ff46dd16094
Sinhika <sinhika@republicofnewhome.org>
2021-05-31 16:41:55 -0500

    refactored config

--- src/main/java/mod/akkamaddi/classicalalchemy/config/ClassicalAlchemyConfig.java
+++ src/main/java/mod/akkamaddi/classicalalchemy/config/ClassicalAlchemyConfig.java
@@ -1,35 +1,11 @@
 package mod.akkamaddi.classicalalchemy.config;
 
-import java.util.HashMap;
-import java.util.Map;
+import mod.alexndr.simplecorelib.config.SimpleConfig;
 
-import mod.alexndr.simplecorelib.config.ISimpleConfig;
-
-public class ClassicalAlchemyConfig implements ISimpleConfig
+public class ClassicalAlchemyConfig extends SimpleConfig
 {
-    // recipe flags
-    private static Map<String, Boolean> flags = new HashMap<>();
-    
     // other variables
     public static ClassicalAlchemyConfig INSTANCE = new ClassicalAlchemyConfig();
     public static boolean addChestLoot;
     
-    @Override
-    public void clear()
-    {
-        flags.clear();
-    }
-
-    @Override
-    public boolean getFlag(String n)
-    {
-        Boolean obj = flags.get(n);
-        return obj != null && obj;
-    }
-
-    @Override
-    public void putFlag(String n, boolean val)
-    {
-        flags.put(n, val);
-    }
 } // end class
