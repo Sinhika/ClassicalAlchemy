c6277de481cb5460c5d6c737c73a45c92d014f1e
Sinhika <sinhika@republicofnewhome.org>
2021-07-20 14:27:19 -0500

    updated forge & dependencies; changed ModTags to use TagUtils instead of reinventing the wheel

--- src/main/java/mod/akkamaddi/classicalalchemy/init/ModTags.java
+++ src/main/java/mod/akkamaddi/classicalalchemy/init/ModTags.java
@@ -1,21 +1,18 @@
 package mod.akkamaddi.classicalalchemy.init;
 
+import mod.alexndr.simplecorelib.helpers.TagUtils;
 import net.minecraft.item.Item;
 import net.minecraft.tags.ITag;
-import net.minecraft.tags.ItemTags;
 
 public class ModTags
 {
     public static class Items
     {
-        public static final ITag.INamedTag<Item> INGOTS_COPPER = forgeTag("ingots/copper");
-        public static final ITag.INamedTag<Item> INGOTS_TIN = forgeTag("ingots/tin");
-        public static final ITag.INamedTag<Item> INGOTS_ARSENIDE_BRONZE = forgeTag("ingots/arsenide_bronze");
-        public static final ITag.INamedTag<Item> REAGENT_OLD_LACE = forgeTag("reagents/old_lace");
+        public static final ITag.INamedTag<Item> INGOTS_COPPER = TagUtils.forgeTag("ingots/copper");
+        public static final ITag.INamedTag<Item> INGOTS_TIN = TagUtils.forgeTag("ingots/tin");
+        public static final ITag.INamedTag<Item> INGOTS_ARSENIDE_BRONZE = TagUtils.forgeTag("ingots/arsenide_bronze");
+        public static final ITag.INamedTag<Item> REAGENT_OLD_LACE = TagUtils.forgeTag("reagents/old_lace");
         
-        private static ITag.INamedTag<Item> forgeTag(String name) {
-            return ItemTags.bind("forge:" + name);
-        }
     } // end class Items
 
 } // end class
